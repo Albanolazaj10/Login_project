703254ba0dee217b9445fe5ed0720c268bdac8e4
#AlbAnO#(^_^) <58682823+Albanolazaj10@users.noreply.github.com>
2021-02-09 23:56:20 +0100

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb://albano123:albano123@test-cluster1-shard-00-00.ytacy.mongodb.net:27017,test-cluster1-shard-00-01.ytacy.mongodb.net:27017,test-cluster1-shard-00-02.ytacy.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-4jz29r-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
